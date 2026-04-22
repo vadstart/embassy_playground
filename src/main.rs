24bea4e544c57432e4367ea5741e8b1f0c74c348
vadstart <vaderxv@gmail.com>
2026-04-22 12:20:37 +0200

    embassy_sync + led row button controls

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,74 @@
 
 use embassy_executor::Spawner;
 use embassy_rp::gpio::{AnyPin, Input, Level, Output, Pull};
+use embassy_rp::Peri;
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::signal::Signal;
 use embassy_time::Timer;
-use defmt::info;
+use defmt::{expect, info};
 use { defmt_rtt as _, panic_probe as _ };
 
-// 1. create executor(s)
-// 2. spawn task(s)
-// 3. run executor
+#[derive(Clone, Copy)]
+enum Button {
+    Left,
+    Right,
+}
+
+static SIGNAL: Signal<ThreadModeRawMutex, Button> = Signal::new();
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("Starting the app...");
 
     let p = embassy_rp::init(Default::default());
-    let mut led_red: Output<'_> = Output::new(p.PIN_15, Level::Low);
+    let leds: [Output<'static>; 3] = [
+        Output::new(p.PIN_15, Level::Low),
+        Output::new(p.PIN_17, Level::Low),
+        Output::new(p.PIN_16, Level::Low)
+    ];
 
-    spawner.spawn(button(p.PIN_12.degrade(), "A")).unwrap();
+    _spawner.spawn(led_controller(leds).unwrap());
+    _spawner.spawn(button(p.PIN_12.into(), Button::Left).unwrap());
+    _spawner.spawn(button(p.PIN_14.into(), Button::Right).unwrap());
+}
+
+#[embassy_executor::task]
+async fn led_controller(mut leds: [Output<'static>; 3]) {
+    let mut index = 0;
+
+    loop {
+        let event = SIGNAL.wait().await;
+
+        match event {
+            Button::Left => {
+                index = (index + 2) % 3;
+            }
+            Button::Right => {
+                index = (index + 1) % 3;
+            }
+        }
+
+        for (i,led) in leds.iter_mut().enumerate() {
+            if i == index {
+                led.set_high();
+            } else {
+                led.set_low();
+            }
+        }
+    }
 }
 
 #[embassy_executor::task(pool_size = 2)]
-async fn button(pin: AnyPin, id: &'static str) {
-    let mut button: Input<'_> = Input::new(pin, Pull::None);
+async fn button(pin: Peri<'static, AnyPin>, dir: Button) {
+    let mut button: Input<'static> = Input::new(pin, Pull::None);
 
     loop {
         button.wait_for_low().await;
-        led_red.set_high();
+        info!("Button {} pressed!", dir);
+        SIGNAL.signal(dir);
+        // led.set_high();
         Timer::after_millis(200).await; // Debounce
         button.wait_for_high().await;
-        led_red.set_low();
+        // led.set_low();
     }
 }
